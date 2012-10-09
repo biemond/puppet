94da4be1f276ac2e7e23c59575eb4cab86e4d984
biemond <biemond@gmail.com>
2012-10-09 22:14:28 +0200

    oracle facter

--- modules/wls/lib/facter/oracle_middleware_homes.rb
+++ modules/wls/lib/facter/oracle_middleware_homes.rb
@@ -1,44 +1,102 @@
 # oracle_middleware_homes.rb
 
+# read middleware home in the oracle home folder
+def get_homes()
+  if ["centos", "redhat","OracleLinux","ubuntu","debian"].include?Facter.value(:operatingsystem)
+    if FileTest.exists?("/home/oracle/bea/beahomelist")
+      output = File.read("/home/oracle/bea/beahomelist")
+      return output.split(/;/)
+    end
+  end
+end
 
+# read middleware home in the oracle home folder
+def get_domain(name)
+  if ["centos", "redhat","OracleLinux","ubuntu","debian"].include?Facter.value(:operatingsystem)
+    output = Facter::Util::Resolution.exec('/bin/ls '+name+'/admin').chomp
+    return output.split(/\r?\n/)
+  end
+end
 
-if ["centos", "redhat","OracleLinux","ubuntu","debian"].include?Facter.value(:operatingsystem)
-  if FileTest.exists?("/home/oracle/bea/beahomelist")
-    output = File.read("/home/oracle/bea/beahomelist")
-    output.split(/;/).each_with_index do |item, i|
-      Facter.add("oracle_middleware_homes_#{i}") do
-        setcode do
-          "#{item}"
-        end
-      end
-    end  
+def get_nodemanagers()
+  if ["centos", "redhat","OracleLinux","ubuntu","debian"].include?Facter.value(:operatingsystem)
+    output = Facter::Util::Resolution.exec("/bin/ps -eo pid,cmd | /bin/grep -i nodemanager.javahome | /bin/grep -v grep | awk ' {print \"pid:\", substr(\$0,0,5), \"port:\" , substr(\$0,index(\$0,\"-DListenPort\")+13,4) } '").chomp
+    return output.split(/\r?\n/)
+  end
+end
+
+def get_wlsservers()
+  if ["centos", "redhat","OracleLinux","ubuntu","debian"].include?Facter.value(:operatingsystem)
+    output = Facter::Util::Resolution.exec("/bin/ps -eo pid,cmd | /bin/grep -i weblogic.server | /bin/grep -v grep | awk ' {print \"pid:\", substr(\$0,0,5), \"name:\" ,substr(\$0,index(\$0,\"weblogic.Name\")+14,12) }'").chomp
+    return output.split(/\r?\n/)
   end
 end
 
 
-Facter.add("oracle_middleware_homes") do
-  confine :kernel => :linux
+# report all oracle homes / domains
+get_homes.each_with_index do |mdw, i|
+  Facter.add("ora_mdw_#{i}") do
+    setcode do
+      mdw
+    end
+  end
+  get_domain(mdw).each_with_index do |domain, n|
+    Facter.add("ora_mdw_#{i}_domain_#{n}") do
+      setcode do
+        domain
+      end
+    end
+  end
+  Facter.add("ora_mdw_#{i}_domain_cnt") do
+    setcode do
+     count = get_domain(mdw).count
+     if count.nil?
+       count = 0
+     end
+     count
+    end
+  end
+end 
+
+# all homes on 1 row
+Facter.add("ora_mdw_homes") do
   setcode do
-    if ["centos", "redhat","OracleLinux","ubuntu","debian"].include?Facter.value(:operatingsystem)
-      if FileTest.exists?("/home/oracle/bea/beahomelist")
-        output = File.read("/home/oracle/bea/beahomelist")
-      else
-        "not found"
-      end
-    end 
+    str = ""
+    get_homes.each do |item|
+      str += item + " -- "
+    end
+    str	
   end
 end
 
-Facter.add("oracle_middleware_homes_count") do
-  confine :kernel => :linux
+# all nodemanager 
+get_nodemanagers.each_with_index do |node, i|
+  Facter.add("ora_node_mgr_#{i}") do
+    setcode do
+      node
+    end
+  end
+end 
+
+# report all weblogic servers
+get_wlsservers.each_with_index do |wls, i|
+  Facter.add("ora_wls_#{i}") do
+    setcode do
+      wls
+    end
+  end
+end 
+
+
+# all home counter
+Facter.add("ora_mdw_cnt") do
   setcode do
-    count = 0
-    if ["centos", "redhat","OracleLinux","ubuntu","debian"].include?Facter.value(:operatingsystem)
-      if FileTest.exists?("/home/oracle/bea/beahomelist")
-        output = File.read("/home/oracle/bea/beahomelist")
-        count = output.split(/;/).count
-      end
-    end 
-    count
+     count = get_homes.count
+     if count.nil?
+       count = 0
+     end
+     count
   end
 end
+
+
