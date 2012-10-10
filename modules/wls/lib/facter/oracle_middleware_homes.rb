dc08c97406ab35f96c1ec7b26e3bb3498b3822d3
biemond <biemond@gmail.com>
2012-10-10 21:47:07 +0200

    fixes oracle fact so it works everywhere

--- modules/wls/lib/facter/oracle_middleware_homes.rb
+++ modules/wls/lib/facter/oracle_middleware_homes.rb
@@ -6,6 +6,8 @@ def get_homes()
     if FileTest.exists?("/home/oracle/bea/beahomelist")
       output = File.read("/home/oracle/bea/beahomelist")
       return output.split(/;/)
+    else
+      return nil
     end
   end
 end
@@ -13,50 +15,70 @@ end
 # read middleware home in the oracle home folder
 def get_domain(name)
   if ["centos", "redhat","OracleLinux","ubuntu","debian"].include?Facter.value(:operatingsystem)
-    output = Facter::Util::Resolution.exec('/bin/ls '+name+'/admin').chomp
-    return output.split(/\r?\n/)
+    if FileTest.exists?(name+'/admin')
+      output = Facter::Util::Resolution.exec('/bin/ls '+name+'/admin')
+      if output.nil?
+        return nil
+      else 
+        return output.split(/\r?\n/)
+      end
+    else
+      return nil  
+    end
   end
 end
 
 def get_nodemanagers()
   if ["centos", "redhat","OracleLinux","ubuntu","debian"].include?Facter.value(:operatingsystem)
-    output = Facter::Util::Resolution.exec("/bin/ps -eo pid,cmd | /bin/grep -i nodemanager.javahome | /bin/grep -v grep | awk ' {print \"pid:\", substr(\$0,0,5), \"port:\" , substr(\$0,index(\$0,\"-DListenPort\")+13,4) } '").chomp
-    return output.split(/\r?\n/)
+    output = Facter::Util::Resolution.exec("/bin/ps -eo pid,cmd | /bin/grep -i nodemanager.javahome | /bin/grep -v grep | awk ' {print \"pid:\", substr(\$0,0,5), \"port:\" , substr(\$0,index(\$0,\"-DListenPort\")+13,4) } '")
+    if output.nil?
+      return nil
+    else 
+      return output.split(/\r?\n/)
+    end
   end
 end
 
 def get_wlsservers()
   if ["centos", "redhat","OracleLinux","ubuntu","debian"].include?Facter.value(:operatingsystem)
-    output = Facter::Util::Resolution.exec("/bin/ps -eo pid,cmd | /bin/grep -i weblogic.server | /bin/grep -v grep | awk ' {print \"pid:\", substr(\$0,0,5), \"name:\" ,substr(\$0,index(\$0,\"weblogic.Name\")+14,12) }'").chomp
-    return output.split(/\r?\n/)
+    output = Facter::Util::Resolution.exec("/bin/ps -eo pid,cmd | /bin/grep -i weblogic.server | /bin/grep -v grep | awk ' {print \"pid:\", substr(\$0,0,5), \"name:\" ,substr(\$0,index(\$0,\"weblogic.Name\")+14,12) }'")
+    if output.nil?
+      return nil 
+    else 
+      return output.split(/\r?\n/)
+    end
   end
 end
 
-
 # report all oracle homes / domains
-get_homes.each_with_index do |mdw, i|
-  Facter.add("ora_mdw_#{i}") do
-    setcode do
-      mdw
-    end
-  end
-  get_domain(mdw).each_with_index do |domain, n|
-    Facter.add("ora_mdw_#{i}_domain_#{n}") do
+unless get_homes.nil?
+  get_homes.each_with_index do |mdw, i|
+    Facter.add("ora_mdw_#{i}") do
       setcode do
-        domain
+        mdw
       end
     end
-  end
-  Facter.add("ora_mdw_#{i}_domain_cnt") do
-    setcode do
-     count = get_domain(mdw).count
-     if count.nil?
-       count = 0
-     end
-     count
+    unless get_domain(mdw).nil?
+      get_domain(mdw).each_with_index do |domain, n|
+        Facter.add("ora_mdw_#{i}_domain_#{n}") do
+          setcode do
+            domain
+          end
+        end
+      end
     end
-  end
-end 
+    Facter.add("ora_mdw_#{i}_domain_cnt") do
+      setcode do
+        if get_domain(mdw).nil?
+          count = 0
+        else        
+          count = get_domain(mdw).count
+        end
+        count
+      end
+    end
+  end 
+end
 
 # all homes on 1 row
 Facter.add("ora_mdw_homes") do
@@ -69,33 +91,38 @@ Facter.add("ora_mdw_homes") do
   end
 end
 
+
 # all nodemanager 
-get_nodemanagers.each_with_index do |node, i|
-  Facter.add("ora_node_mgr_#{i}") do
-    setcode do
-      node
+unless get_nodemanagers.nil?
+  get_nodemanagers.each_with_index do |node, a|
+    Facter.add("ora_node_mgr_#{a}") do
+      setcode do
+        node
+      end
     end
-  end
-end 
+  end 
+end
 
 # report all weblogic servers
-get_wlsservers.each_with_index do |wls, i|
-  Facter.add("ora_wls_#{i}") do
-    setcode do
-      wls
+unless get_wlsservers.nil?
+  get_wlsservers.each_with_index do |wls, i|
+    Facter.add("ora_wls_#{i}") do
+      setcode do
+        wls
+      end
     end
-  end
-end 
-
+  end 
+end
 
 # all home counter
 Facter.add("ora_mdw_cnt") do
   setcode do
-     count = get_homes.count
-     if count.nil?
-       count = 0
-     end
-     count
+    homes = get_homes
+    if homes.nil?
+      count = 0
+    else
+      count = homes.count
+    end
   end
 end
 
