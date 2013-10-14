f2e2099aceafbf17e0cbb62ab6c6ce56446f991c
Edwin Biemond <biemond@gmail.com>
2013-10-14 20:17:10 +0200

    added extra config for vagrant

--- modules/orautils/manifests/params.pp
+++ modules/orautils/manifests/params.pp
@@ -1,110 +1,117 @@
 class orautils::params {
 
-  $osOracleHome = $::hostname ? { 
-                                    devagent1  => "/opt/oracle/wls",
-                                    devagent10 => "/opt/oracle/wls",
-                                    devagent30 => "/opt/oracle/wls",
-                                    devagent31 => "/opt/oracle",
-                                    devagent40 => "/opt/oracle/wls",
-                                    devagent41 => "/opt/oracle/wls",
-                                    wls12      => "/oracle/product",
-                                    soabeta2   => "/opt/oracle/wls",
-                                    default    => "/opt/wls", 
+  $osOracleHome = $::hostname ? {
+                                    devagent1    => "/opt/oracle/wls",
+                                    devagent10   => "/opt/oracle/wls",
+                                    devagent30   => "/opt/oracle/wls",
+                                    devagent31   => "/opt/oracle",
+                                    devagent40   => "/opt/oracle/wls",
+                                    devagent41   => "/opt/oracle/wls",
+                                    wls12        => "/oracle/product",
+                                    "vagrantoel64.example.com" => "/oracle/product",
+                                    soabeta2     => "/opt/oracle/wls",
+                                    default      => "/opt/wls",
                                 }
 
-  $oraInventory = $::hostname ? { 
-                                    devagent1  => "/opt/oracle/oraInventory",
-                                    devagent10 => "/opt/oracle/oraInventory",
-                                    devagent30 => "/opt/oracle/oraInventory",
-                                    devagent31 => "/opt/oracle/oraInventory",
-                                    devagent40 => "/opt/oracle/oraInventory",
-                                    devagent41 => "/opt/oracle/oraInventory",
-                                    soabeta2   => "/home/oracle/soabetainv",
-                                    wls12      => "/oracle/oraInventory",
-                                    default    => "/opt/wls/oraInventory", 
+  $oraInventory = $::hostname ? {
+                                    devagent1    => "/opt/oracle/oraInventory",
+                                    devagent10   => "/opt/oracle/oraInventory",
+                                    devagent30   => "/opt/oracle/oraInventory",
+                                    devagent31   => "/opt/oracle/oraInventory",
+                                    devagent40   => "/opt/oracle/oraInventory",
+                                    devagent41   => "/opt/oracle/oraInventory",
+                                    soabeta2     => "/home/oracle/soabetainv",
+                                    wls12        => "/oracle/oraInventory",
+                                    "vagrantoel64.example.com" => "/oracle/oraInventory",
+                                    default      => "/opt/wls/oraInventory",
                                 }
 
 
 
   $osDomainType = $::hostname ? {
-                                    devagent30 => "soa",
-                                    devagent31 => "admin",
-                                    wls12      => "admin",
-                                    devagent40 => "admin",
-                                    devagent41 => "admin",
-                                    soabeta2   => "soa",
-                                    default    => "web", 
+                                    devagent30   => "soa",
+                                    devagent31   => "admin",
+                                    wls12        => "admin",
+                                    "vagrantoel64.example.com" => "admin",
+                                    devagent40   => "admin",
+                                    devagent41   => "admin",
+                                    soabeta2     => "soa",
+                                    default      => "web",
                                 }
 
   $osLogFolder = $::hostname ? {
-                                      default    => "/data/logs", 
+                                      default    => "/data/logs",
                                }
 
 
   $osDownloadFolder = $::hostname ? {
-  	                                  devagent1  => "/data/install/oracle", 
-                                      default    => "/data/install", 
+  	                                  devagent1  => "/data/install/oracle",
+                                      default    => "/data/install",
                                      }
 
 	$shell        = $::operatingsystem ? { Solaris => "!/usr/bin/ksh",
   															         default => "!/bin/sh",
-  															       }   
+  															       }
 
-  $osMdwHome     = $::hostname ?  { wls12       => "${osOracleHome}/Middleware12c",
-                                    devagent31  => "${osOracleHome}/Middleware12c",
-                                    soabeta2    => "${osOracleHome}/Middleware12c",
-                                    default     => "${osOracleHome}/Middleware11gR1",
-                                  }   
+  $osMdwHome     = $::hostname ?  { wls12        => "${osOracleHome}/Middleware12c",
+                                    "vagrantoel64.example.com" => "${osOracleHome}/Middleware12c",
+                                    devagent31   => "${osOracleHome}/Middleware12c",
+                                    soabeta2     => "${osOracleHome}/Middleware12c",
+                                    default      => "${osOracleHome}/Middleware11gR1",
+                                  }
 
-  $osWlHome     = $::hostname ?  { wls12      => "${osOracleHome}/Middleware12c/wlserver",
-                                   devagent31 => "${osOracleHome}/Middleware12c/wlserver",
-                                   soabeta2   => "${osOracleHome}/Middleware12c/wlserver",
-                                   default    => "${osOracleHome}/Middleware11gR1/wlserver_10.3",
-                                 }   
+  $osWlHome     = $::hostname ?  { wls12        => "${osOracleHome}/Middleware12c/wlserver",
+                                   "vagrantoel64.example.com" => "${osOracleHome}/Middleware12c/wlserver",
+                                   devagent31   => "${osOracleHome}/Middleware12c/wlserver",
+                                   soabeta2     => "${osOracleHome}/Middleware12c/wlserver",
+                                   default      => "${osOracleHome}/Middleware11gR1/wlserver_10.3",
+                                 }
 
   $oraUser      = $::hostname ? { default => "oracle", }
 
   $userHome     = $::operatingsystem ? { Solaris => "/export/home",
-  															         default => "/home", 
+  															         default => "/home",
   															       }
   $oraInstHome  = $::operatingsystem ? { Solaris => "/var/opt",
-  															         default => "/etc", 
+  															         default => "/etc",
   															       }
 
-  $osDomain     = $::hostname ? {   wls12      => "Wls12c",
-                                    soabeta2   => "soabeta2", 
-                                    devagent31 => "Wls12c", 
-                                    devagent40 => "WlsDomain", 
-                                    devagent41 => "WlsDomain", 
-                                    default    => "osbSoaDomain", 
-                                }
-                                
-  $osDomainPath = $::hostname ? { 
-                                    default    => "${osMdwHome}/user_projects/domains/${osDomain}", 
+  $osDomain     = $::hostname ? {   wls12        => "Wls12c",
+                                    "vagrantoel64.example.com" => "Wls12c",
+                                    soabeta2     => "soabeta2",
+                                    devagent31   => "Wls12c",
+                                    devagent40   => "WlsDomain",
+                                    devagent41   => "WlsDomain",
+                                    default      => "osbSoaDomain",
                                 }
 
-  $nodeMgrPath = $::hostname ? { 
-                                    wls12      => "${osMdwHome}/user_projects/domains/${osDomain}/bin", 
-                                    devagent31 => "${osMdwHome}/user_projects/domains/${osDomain}/bin", 
-                                    soabeta2   => "${osMdwHome}/user_projects/domains/${osDomain}/bin", 
-                                    default    => "${osWlHome}/server/bin", 
+  $osDomainPath = $::hostname ? {
+                                    default    => "${osMdwHome}/user_projects/domains/${osDomain}",
+                                }
+
+  $nodeMgrPath = $::hostname ? {
+                                    wls12        => "${osMdwHome}/user_projects/domains/${osDomain}/bin",
+                                    "vagrantoel64.example.com" => "${osMdwHome}/user_projects/domains/${osDomain}/bin",
+                                    devagent31   => "${osMdwHome}/user_projects/domains/${osDomain}/bin",
+                                    soabeta2     => "${osMdwHome}/user_projects/domains/${osDomain}/bin",
+                                    default      => "${osWlHome}/server/bin",
                                 }
 
 
-  $nodeMgrPort = $::hostname ?  { 
-                                    default    => "5556", 
-                                }                                 
+  $nodeMgrPort = $::hostname ?  {
+                                    default    => "5556",
+                                }
 
-  $wlsUser     = $::hostname ?  { 
-                                    default    => "weblogic", 
-                                }                                 
+  $wlsUser     = $::hostname ?  {
+                                    default    => "weblogic",
+                                }
 
-  $wlsPassword = $::hostname ?  { 
-                                    default    => "weblogic1", 
-                                }       
+  $wlsPassword = $::hostname ?  {
+                                    default    => "weblogic1",
+                                }
 
-  $wlsAdminServer = $::hostname ?  { 
-                                    default    => "AdminServer", 
-                                }       
+  $wlsAdminServer = $::hostname ?  {
+                                    default    => "AdminServer",
+                                }
 
 }
