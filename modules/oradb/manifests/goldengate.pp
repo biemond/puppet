2b98402d322707f2bd3d9094f70cafbd416b5754
Edwin Biemond <biemond@gmail.com>
2014-03-28 19:32:24 +0100

    puppet lint

--- modules/oradb/manifests/goldengate.pp
+++ modules/oradb/manifests/goldengate.pp
@@ -1,3 +1,6 @@
+#
+#
+#
 define oradb::goldengate( $version                 = '12.1.2',
                           $file                    = undef,
                           $tarFile                 = undef,     # only for < 12.1.2
@@ -61,10 +64,10 @@ define oradb::goldengate( $version                 = '12.1.2',
         group       => $group,
       }
 
-	    oradb::utils::orainst{"ggate orainst ${version}":
-	      ora_inventory_dir => $oraInventory,
-	      os_group          => $group,
-	    }
+      oradb::utils::orainst{"ggate orainst ${version}":
+        ora_inventory_dir => $oraInventory,
+        os_group          => $group,
+      }
       
       exec { "install oracle goldengate":
           command     => "/bin/sh -c 'unset DISPLAY;${downloadDir}/${ggateInstallDir}/Disk1/runInstaller -silent -waitforcompletion -responseFile ${downloadDir}/oggcore.rsp'",
@@ -105,7 +108,7 @@ define oradb::goldengate( $version                 = '12.1.2',
         ensure        => directory,
         recurse       => false,
         replace       => false,
-        mode          => 0775,
+        mode          => '0775',
         owner         => $user,
         group         => $group,
       }
